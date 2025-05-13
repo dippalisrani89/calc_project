596a84da53a264455e81802d2760f9f00361ea5d
Dippal <dippalisrani89@gmail.com>
2025-05-13 16:02:59 +0530

    Add modulus features

--- calc.py
+++ calc.py
@@ -3,10 +3,13 @@ def add(x,y):
 
 def sub(x,y):
     return x-y
+def mod(x,y):
+    return x%y
 
 
 
 if __name__ == "__main__":
     print(add(10,20)) 
     print(sub(10,20))
+    print(mod(10,20))
     
